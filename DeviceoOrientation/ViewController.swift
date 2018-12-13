1741b49e5cbd5f42ba00ad57ae0eb013c3c908a6
Hameed Abdullah <hameedabdullah@hameeds-macbook-pro.local>
2018-12-12 19:11:02 -0500

    Initial Commit

---
+++ DeviceoOrientation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeviceoOrientation
+//
+//  Created by Hameed Abdullah on 12/12/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
