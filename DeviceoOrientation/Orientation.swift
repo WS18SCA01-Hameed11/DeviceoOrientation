7e844617ccd8b286611b164348312532cdcb8cdf
Hameed Abdullah <hameedabdullah@Hameeds-MacBook-Pro.local>
2018-12-15 01:19:34 -0500

    second commit

---
+++ DeviceoOrientation/Orientation.swift
@@ -0,0 +1,15 @@
+//
+//  Orientation.swift
+//  DeviceoOrientation
+//
+//  Created by Hameed Abdullah on 12/15/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Orientation {
+    let name: String
+    let color: UIColor
+}
--- DeviceoOrientation/ViewController.swift
+++ DeviceoOrientation/ViewController.swift
@@ -12,16 +12,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     
+    let names: [Orientation] = [
+        Orientation(name: "unknown", color: .black),
+        Orientation(name: "portrait", color: .red),
+        Orientation(name: "portraitUpsideDown", color: .green),
+        Orientation(name: "landscapeLeft", color: .blue),
+        Orientation(name: "landscapeRight", color: .orange),
+        Orientation(name: "faceUp", color: .gray),
+        Orientation(name: "faceDown", color: .darkGray)
+        
+    ]
     
-    let names: [String] = [   //Pull down Hardware -> Orientation
-        "unknown",            //0
-        "portrait",           //1 home button at bottom
-        "portraitUpsideDown", //2 home button at top
-        "landscapeLeft",      //3 home button on right
-        "landscapeRight",     //4 home button on left
-        "faceUp",             //5 screen facing upwards
-        "faceDown",           //6 screen facing downwards
-    ];
+//    let names: [String] = [   //Pull down Hardware -> Orientation
+//        "unknown",            //0
+//        "portrait",           //1 home button at bottom
+//        "portraitUpsideDown", //2 home button at top
+//        "landscapeLeft",      //3 home button on right
+//        "landscapeRight",     //4 home button on left
+//        "faceUp",             //5 screen facing upwards
+//        "faceDown",           //6 screen facing downwards
+//    ];
     
     let colors: [UIColor] = [.red, .black, .gray, .green, .blue, .orange, .darkGray]
     
@@ -46,14 +56,15 @@ class ViewController: UIViewController {
     func updateUI() {
         //currentIndex %= colors.count;
         
-        if currentIndex == colors.count {
+        if currentIndex == names.count {
             currentIndex = 0
         }
         
-        label.text = names[UIDevice.current.orientation.rawValue]
-        view.backgroundColor = colors[currentIndex]
+        label.text = names[UIDevice.current.orientation.rawValue].name
+        view.backgroundColor = names[currentIndex].color
     }
 
+    
 }
 
 
